a0e07fe318c1d3f8933b5f8cc75eb62f365b81ab
Bola Milad <91290835+BMS250@users.noreply.github.com>
2026-05-19 01:03:17 +0300

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cs
@@ -0,0 +1,67 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> freq = [];
+        // Convert it to Dictionary<int, HashSet<int>> and save max freq
+        List<HashSet<int>> freqNums = [[]];
+        List<int> result = [];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!freq.TryGetValue(nums[i], out var value))
+            {
+                freq[nums[i]] = 1;
+                // try
+                // {
+                //     freqNums[1].Add(nums[i]);
+                // }
+                // catch
+                // {
+                //     freqNums.Insert(1, [nums[i]]);
+                // }
+                if (freqNums.Count > 1)
+                    freqNums[1].Add(nums[i]);
+                else
+                    freqNums.Insert(1, [nums[i]]);
+            }
+            else
+            {
+                freqNums[freq[nums[i]]].Remove(nums[i]);
+                freq[nums[i]]++;
+                // try
+                // {
+                //     freqNums[freq[nums[i]]].Add(nums[i]);
+                // }
+                // catch
+                // {
+                //     freqNums.Insert(freq[nums[i]], [nums[i]]);
+                // }
+                int newFreq = freq[nums[i]];
+                if (freqNums.Count > newFreq)
+                    freqNums[newFreq].Add(nums[i]);
+                else
+                    freqNums.Insert(newFreq, [nums[i]]);
+            }
+        }
+        int remaining = k;
+        for (int i = freqNums.Count - 1; remaining > 0 && i > 0; i--)
+        {
+            if (freqNums[i] == null) continue;
+            List<int> temp = [.. freqNums[i]];
+            if (freqNums[i].Count <= remaining)
+            {
+                result.AddRange(temp);
+                remaining -= temp.Count;
+            }
+            else
+            {
+                int j = 0;
+                for (; j < remaining; j++)
+                {
+                    result.Add(temp[j]);
+                }
+                remaining -= j;
+            }
+        }
+        return result.ToArray();
+    }
+}
