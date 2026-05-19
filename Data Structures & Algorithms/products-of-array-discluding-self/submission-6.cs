c73a74cb6af295f4e9787330a5ba33300fb314f1
Bola Milad <91290835+BMS250@users.noreply.github.com>
2026-05-20 00:50:29 +0300

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int temp = 1;
+        int[] result = new int[nums.Length];
+        result[0] = result[result.Length - 1] = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            temp *= nums[i + 1];
+            result[i] *= temp;
+        }
+        return result;
+    }
+}
