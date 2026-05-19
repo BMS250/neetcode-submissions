4e6518cb804f456f1aad7c79d5386b50f895a874
Bola Milad <91290835+BMS250@users.noreply.github.com>
2026-05-19 23:07:13 +0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        var result = new StringBuilder();
+        foreach (var str in strs)
+        {
+            result.Append($"{str.Length}#{str}");
+        }
+        return result.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> strs = [];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (int.TryParse(s[i].ToString(), out int num))
+            {
+                while (int.TryParse(s[++i].ToString(), out int value))
+                {
+                    num = num * 10 + value;
+                }
+                if (s[i] == '#')
+                {
+                    strs.Add(s[(i+1)..(i+1+num)]);
+                    i += num;
+                }
+            }
+        }
+        return strs;
+   }
+}
