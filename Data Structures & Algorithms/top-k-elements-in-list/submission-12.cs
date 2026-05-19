9cc5e3885a31e95426328b569438d1f075d64797
Bola Milad <91290835+BMS250@users.noreply.github.com>
2026-05-19 21:22:29 +0300

    Add: top-k-elements-in-list - submission-12

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-12.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k)
+{
+    Dictionary<int, int> freq = [];
+    foreach (int num in nums)
+    {
+        freq.TryGetValue(num, out int count);
+        freq[num] = count + 1;
+    }
+
+    int maxFreq = 0;
+    // Cap at distinct count, not nums.Length
+    foreach (int count in freq.Values)
+        if (count > maxFreq) maxFreq = count;
+
+    List<int>[] buckets = new List<int>[maxFreq + 1]; // much smaller!
+    foreach (var (num, count) in freq)
+    {
+        buckets[count] ??= [];
+        buckets[count].Add(num);
+    }
+
+    int[] result = new int[k];
+    int idx = 0;
+    for (int i = buckets.Length - 1; i >= 1 && idx < k; i--)
+    {
+        if (buckets[i] is null) continue;
+        foreach (int num in buckets[i])
+        {
+            result[idx++] = num;
+            if (idx == k) break;
+        }
+    }
+    return result;
+}
+}
